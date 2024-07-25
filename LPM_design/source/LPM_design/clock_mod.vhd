6f3c3f35aefd4cca71f70f027b9d9beaa9c003b2
David Holmes <david.holmes@kpnmail.nl>
2024-07-25 11:17:05 +0200

    Clock Module working

--- LPM_design/source/LPM_design/clock_mod.vhd
+++ LPM_design/source/LPM_design/clock_mod.vhd
@@ -24,13 +24,13 @@ architecture Behavioral of clk_mod is
 
 --SIGNALS AND STATES
 signal ADC_CNT: std_logic_vector(7 downto 0);
-signal ADC_CNT_MAX: std_logic_vector(7 downto 0);
+signal ADC_HALF_CNT_MAX: std_logic_vector(7 downto 0);
+signal ADC_FULL_CNT_MAX: std_logic_vector(7 downto 0);
 signal ADC_clk: std_logic;
 signal AGC_CNT: std_logic_vector(7 downto 0);
 signal AGC_CNT_MAX: std_logic_vector(7 downto 0);
 signal AGC_clk: std_logic;
-signal XCORR_CNT: std_logic_vector(2 downto 0);
-constant XCORR_CNT_MAX: std_logic_vector(2 downto 0) := "111";
+signal XCORR_CLK_ACTIVE: std_logic;
 type t_state is (S_IDLE, S_RUN, S_WAIT);
 signal state: t_state;
 
@@ -40,7 +40,7 @@ begin
 hf_clk_o <= hf_clk_i;
 ADC_clk_o <= ADC_clk;
 AGC_clk_o <= AGC_clk;
-XCORR_clk_o <= hf_clk_i when state = S_RUN else '0';
+XCORR_clk_o <= hf_clk_i and XCORR_CLK_ACTIVE;
 
 --AGC CLOCK
 P_AGC_CLK: process(rst_i, hf_clk_i) begin
@@ -64,52 +64,45 @@ end process P_AGC_CLK;
 P_ADC_CLK: process(rst_i, hf_clk_i) begin
 	if rst_i = '1' then
 		ADC_CNT <= (others => '0');
-		--DIV BY TWO
-		ADC_CNT_MAX <= ("0" & set_per_ADC_i(7 downto 1) - 1);
 		ADC_clk <= '0';
+		ADC_HALF_CNT_MAX <= ("0" & set_per_ADC_i(7 downto 1) - 1);
+		ADC_FULL_CNT_MAX <= set_per_ADC_i-1;
 	elsif rising_edge(hf_clk_i) then
 		--GENERATE CLOCK
-		if ADC_CNT = ADC_CNT_MAX then
-			ADC_clk <= not ADC_clk;
-			ADC_CNT <= (others => '0');
-		else
-			ADC_CNT <= ADC_CNT + 1;
+		ADC_CNT <=ADC_CNT +1;
+		
+		if ADC_CNT >=0 and  ADC_CNT<= ADC_HALF_CNT_MAX then
+			ADC_clk <= '1';
 		end if;
+		
+		if ADC_CNT > ADC_HALF_CNT_MAX and  ADC_CNT< ADC_FULL_CNT_MAX then
+			ADC_clk <= '0';
+		end if;
+		
+		if ADC_CNT=ADC_FULL_CNT_MAX then
+			ADC_CNT <= (others => '0');
+			ADC_clk <= '0';
+		end if;
+
 	end if;
 end process P_ADC_CLK;
 
 --XCORR CLOCK
 P_XCORR_CLK: process(rst_i, hf_clk_i) begin
 	if rst_i = '1' then
-		state <= S_IDLE;
-	elsif rising_edge(hf_clk_i) then
-		case state is
-			
-			--RESET/ IDLE STATE
-			when S_IDLE =>
-				XCORR_CNT <= (others => '0');
-				if ADC_clk = '1' then
-					state <= S_RUN;
-				end if;
-				
-			--FREE RUN MODE FOR 8 CYCLES
-			when S_RUN =>
-				if XCORR_CNT = XCORR_CNT_MAX then
-					state <= S_WAIT;
-				else
-					XCORR_CNT <= XCORR_CNT + 1;
-				end if;
-				
-			--PREVENT A SECOND TRIGGER IN SAME ADC CYLCE
-			when S_WAIT =>
-				if ADC_clk = '0' then
-					state <= S_IDLE;
-				end if;
-			when others =>
-				
-		end case;
+		XCORR_CLK_ACTIVE <= '0';
+	elsif falling_edge(hf_clk_i) then
+
+		if ADC_CNT >=0 and  ADC_CNT<=7  then
+			XCORR_CLK_ACTIVE <= '1';
+		else
+			XCORR_CLK_ACTIVE <= '0';
+		end if;
+		
 	end if;
 end process P_XCORR_CLK;
+
+
 
 end Behavioral;
 
